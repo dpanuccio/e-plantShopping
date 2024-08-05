716f8dc4e9610bc2f0cf4d656c39b79b4dfd0137
dpanuccio <panucciodonna@gmail.com>
2024-08-05 15:34:22 -0400

    Configured Redux store and set up reducers

--- src/store.js
+++ src/store.js
@@ -1,9 +1,16 @@
+// Import configureStore from @reduxjs/toolkit
 import { configureStore } from '@reduxjs/toolkit';
+
+// Import the reducer from CartSlice
 import cartReducer from './CartSlice';
 
- const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-    },
+// Configure the Redux store
+const store = configureStore({
+  reducer: {
+    // Define the slice of state and the associated reducer
+    cart: cartReducer, // 'cart' is the slice of state, and cartReducer is the reducer managing it
+  },
 });
-export default store
+
+// Export the configured store as default
+export default store;
